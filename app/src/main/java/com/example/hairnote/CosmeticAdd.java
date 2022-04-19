8695b5875c1117bdde55da5e3e8f82e3a40344ca
Limiel104 <a.myjak1995@gmail.com>
2022-04-19 12:41:02 +0200

    Added spinners for cosmeticType and cosmeticPehType

--- app/src/main/java/com/example/hairnote/CosmeticAdd.java
+++ app/src/main/java/com/example/hairnote/CosmeticAdd.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,22 +17,52 @@ import java.util.List;
 
 public class CosmeticAdd extends AppCompatActivity {
 
-    private EditText et_cosmeticName, et_cosmeticBrand, et_cosmeticPehType, et_cosmeticType, et_cosmeticInciList, et_cosmeticDesc, et_cosmeticImgPath;
+    private EditText et_cosmeticName, et_cosmeticBrand, et_cosmeticInciList, et_cosmeticDesc, et_cosmeticImgPath;
     Button btn_addCosmetic;
+    AutoCompleteTextView autoCompleteTVPehType, autoCompleteTVCosType;
+    ArrayAdapter adapterPehTypes, adapterCosTypes;
+    String chosenPehType, chosenCosType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cosmetic_add);
 
+        String[] pehTypes = getResources().getStringArray(R.array.PehTypes);
+        String[] cosTypes = getResources().getStringArray(R.array.CosTypes);
+
+        adapterPehTypes = new ArrayAdapter<>(this, R.layout.drop_down_item_cosmetic_peh_type, pehTypes);
+        adapterCosTypes = new ArrayAdapter<>(this, R.layout.drop_down_item_cosmetic_cos_type, cosTypes);
+
+        chosenPehType = "";
+        chosenCosType = "";
+
         btn_addCosmetic = findViewById(R.id.btnAddCosmetic);
         et_cosmeticName = findViewById(R.id.editCosmeticName);
         et_cosmeticBrand = findViewById(R.id.editCosmeticBrand);
-        et_cosmeticPehType = findViewById(R.id.editCosmeticPehType);
-        et_cosmeticType = findViewById(R.id.editCosmeticType);
         et_cosmeticInciList = findViewById(R.id.editCosmeticInciList);
         et_cosmeticDesc = findViewById(R.id.editCosmeticDesc);
         et_cosmeticImgPath = findViewById(R.id.editCosmeticImgPath);
+        autoCompleteTVPehType = findViewById(R.id.autoCompleteTVPehType);
+        autoCompleteTVCosType = findViewById(R.id.autoCompleteTVCosType);
+
+        autoCompleteTVPehType.setAdapter(adapterPehTypes);
+        autoCompleteTVCosType.setAdapter(adapterCosTypes);
+
+        autoCompleteTVPehType.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                chosenPehType = autoCompleteTVPehType.getText().toString();
+            }
+        });
+
+        autoCompleteTVCosType.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                chosenCosType = autoCompleteTVCosType.getText().toString();
+            }
+        });
+
 
         btn_addCosmetic.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,8 +77,8 @@ public class CosmeticAdd extends AppCompatActivity {
                     newCosmetic = new Cosmetic(-1,
                             et_cosmeticName.getText().toString(),
                             et_cosmeticBrand.getText().toString(),
-                            et_cosmeticPehType.getText().toString(),
-                            et_cosmeticType.getText().toString(),
+                            chosenPehType,
+                            chosenCosType,
                             et_cosmeticInciList.getText().toString(),
                             et_cosmeticDesc.getText().toString(),
                             et_cosmeticImgPath.getText().toString(),test);//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
