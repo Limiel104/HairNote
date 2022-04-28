44db43531137a5c431ebf575ae00686be541c1ee
Limiel104 <a.myjak1995@gmail.com>
2022-04-28 12:43:52 +0200

    Added take photo from camera or choose from gallery functionality to CosmeticEdit Activity

--- app/src/main/java/com/example/hairnote/CosmeticEdit.java
+++ app/src/main/java/com/example/hairnote/CosmeticEdit.java
@@ -4,28 +4,48 @@ import static com.example.hairnote.CosmeticDetails.COSMETIC_ID_KEY;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
 public class CosmeticEdit extends AppCompatActivity {
 
-    private EditText edt_cosmeticName, edt_cosmeticBrand, edt_cosmeticDesc, edt_cosmeticImgPath;
-    Button btn_editIngredientsInInciList, btn_edtCosmetic;
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 101;
+
+    private EditText edt_cosmeticName, edt_cosmeticBrand, edt_cosmeticDesc;
+    Button btn_editIngredientsInInciList, btn_edtCosmetic, btn_editCosImg;
     AutoCompleteTextView edt_autoCompleteTVPehType, edt_autoCompleteTVCosType;
     ArrayAdapter adapterPehTypes, adapterCosTypes;
-    String edtPehType, edtCosType;
+    String edtPehType, edtCosType, imgPath;
     String[] edt_pehTypes, edt_cosTypes, edt_listIngredients;
     boolean[] edt_checkedIngredients;
     ArrayList<Integer> edt_chosenIngredients;
@@ -116,6 +136,15 @@ public class CosmeticEdit extends AppCompatActivity {
             }
         });
 
+        btn_editCosImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkAndRequestPermissions(CosmeticEdit.this)) {
+                    chooseImg(CosmeticEdit.this);
+                }
+            }
+        });
+
         btn_edtCosmetic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -130,7 +159,7 @@ public class CosmeticEdit extends AppCompatActivity {
                             edtPehType,
                             edtCosType,
                             edt_cosmeticDesc.getText().toString(),
-                            edt_cosmeticImgPath.getText().toString(),
+                            imgPath,
                             edt_chosenIngredients);
 
                 }catch (Exception e) {
@@ -159,7 +188,7 @@ public class CosmeticEdit extends AppCompatActivity {
         edtPehType = cosmetic.getPehType();
         edtCosType = cosmetic.getCosmeticType();
         edt_cosmeticDesc.setText(cosmetic.getDescription());
-        edt_cosmeticImgPath.setText(cosmetic.getImgPath());
+        imgPath = cosmetic.getImgPath();
 
         Collections.sort(cosmetic.getInciList());
 
@@ -192,10 +221,10 @@ public class CosmeticEdit extends AppCompatActivity {
 
         btn_edtCosmetic = findViewById(R.id.edt_btnEditCosmetic);
         btn_editIngredientsInInciList = findViewById(R.id.edt_btnEditIngredientsInInciList);
+        btn_editCosImg = findViewById(R.id.edt_btnEditCosImg);
         edt_cosmeticName = findViewById(R.id.edt_cosmeticName);
         edt_cosmeticBrand = findViewById(R.id.edt_cosmeticBrand);
         edt_cosmeticDesc = findViewById(R.id.edt_cosmeticDesc);
-        edt_cosmeticImgPath = findViewById(R.id.edt_cosmeticImgPath);
         edt_autoCompleteTVPehType = findViewById(R.id.edt_autoCompleteTVPehType);
         edt_autoCompleteTVCosType = findViewById(R.id.edt_autoCompleteTVCosType);
 
@@ -214,6 +243,132 @@ public class CosmeticEdit extends AppCompatActivity {
             }
         }
         return ingredientID;
+    }
+
+    public static boolean checkAndRequestPermissions(final Activity context){
+
+        int extStoragePermission = ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int cameraPermission = ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA);
+
+        List<String> permissionList = new ArrayList<>();
+
+        if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.CAMERA);
+        }
+
+        if (extStoragePermission != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        if (!permissionList.isEmpty()) {
+            ActivityCompat.requestPermissions(context, permissionList.toArray(new String[permissionList.size()]), REQUEST_ID_MULTIPLE_PERMISSIONS);
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String[] permissions, int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_ID_MULTIPLE_PERMISSIONS:
+                if (ContextCompat.checkSelfPermission(CosmeticEdit.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+
+                }
+                else if (ContextCompat.checkSelfPermission(CosmeticEdit.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+
+                }
+                else {
+                    chooseImg(CosmeticEdit.this);
+                }
+                break;
+        }
+    }
+
+    private void chooseImg(Context context) {
+
+        final CharSequence[] optionsMenu = {"Zrób zdjęcie", "Wybierz z galerii", "Wyjdź"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setItems(optionsMenu, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                if (optionsMenu[i].equals("Zrób zdjęcie")) {
+                    Intent takeImg = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takeImg, 0);
+                }
+                else if (optionsMenu[i].equals("Wybierz z galerii")) {
+                    Intent pickImg = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(pickImg, 1);
+                }
+                else if (optionsMenu[i].equals("Wyjdź")) {
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode != RESULT_CANCELED) {
+
+            switch (requestCode) {
+                case 0:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
+
+                        ContextWrapper contextWrapper = new ContextWrapper(CosmeticEdit.this);
+                        File directory = contextWrapper.getDir("imgs", MODE_PRIVATE);
+
+                        if (!directory.exists()){
+                            directory.mkdir();
+                        }
+
+                        String fileName = new SimpleDateFormat("'Img_'yyyyMMddHHmmss'.jpg'").format(new Date());
+                        File file = new File(directory,fileName);
+                        FileOutputStream fos = null;
+
+                        try {
+                            fos = new FileOutputStream(file);
+                            selectedImage.compress(Bitmap.CompressFormat.JPEG,50,fos);
+                            fos.close();
+
+                        }catch (Exception e){
+                            Log.e("SAVE_IMAGE", e.getMessage(), e);
+                        }
+
+                        imgPath = file.getPath();
+                    }
+                    break;
+                case 1:
+                    if (resultCode == RESULT_OK && data != null) {
+
+                        Uri selectedImg = data.getData();
+                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+                        if (selectedImg != null) {
+                            Cursor cursor = getContentResolver().query(selectedImg, filePathColumn, null, null, null);
+                            if (cursor != null) {
+                                cursor.moveToFirst();
+                                int columnIdx = cursor.getColumnIndex(filePathColumn[0]);
+                                String picturePath = cursor.getString(columnIdx);
+                                Toast.makeText(CosmeticEdit.this,picturePath, Toast.LENGTH_SHORT).show();
+                                imgPath = cursor.getString(columnIdx);
+                                cursor.close();
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
     }
 
 }
